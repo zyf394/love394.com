bbf0341bd11468cf66b158d9ddd33ce70b74ea8d
zhongyufei <zhongyufei@didichuxing.com>
2017-07-01 18:19:41 +0800

    <feat>: 禁止用户缩放页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
     title: 'love394',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
