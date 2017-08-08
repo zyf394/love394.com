1d671f5bf14527ec614ce3c9d95a7ba486ff7de8
zhongyufei <zhongyufei@didichuxing.com>
2017-08-08 23:55:21 +0800

    <fix>(meta): 禁止自动拨电话号码

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+      { name: 'format-detection', content: 'telephone=no' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
